6b484737c32b6b0fddde6f676555bae294ba539a
Natalia Salatiel <ntlsalatieldev@gmail.com>
2024-09-13 11:53:38 -0300

    Primeiro envio

---
+++ index.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello World!"
+
+
+@app.route('/contacts')
+def contacts():
+    return"Diga o que você quer agora!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
